79e477d6185fd80670ee08309bafb04b241f6394
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-09 19:28:37 +0300

    Updated main and Report

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# Convert encoded data to text
-# make Bag of Words (BoW)
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.preprocessing import MinMaxScaler, StandardScaler
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import KFold
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
 import pandas as pd
 
 def read_file(filename):
@@ -77,8 +81,52 @@ def main():
     voc = [str(i) for i in range(8520)]
     # create vectorizer and pass existed corpus
     vectorizer = CountVectorizer(vocabulary=voc)
+    # nparray
     X_bow = vectorizer.transform(X)
     df_bow = pd.DataFrame(X_bow.toarray(), columns=voc)
     df_bow['y'] = y
+    
+    ################ NORMALIZATION ########################################
+    
+    # normalizer = MinMaxScaler()
+    # print(normalizer.fit_transform(df_bow))
+
+    ################ STANDARDIZATION ########################################
+
+    # standardizer = StandardScaler()
+    #print(standardizer.fit_transform(df_bow))
+    
+
+    #################################### CROSS VALIDATION ################################
+    # X_train, X_test, y_train, y_test = train_test_split(df_bow.iloc[:, :-1], df_bow['y'], test_size=0.4, random_state=0)
+    # Split the data to training and testing data 5-Fold
+
+    kfold = KFold(n_splits=5, shuffle=True)
+    rmseList = []
+    rrseList = []
+
+    for i, (train, test) in enumerate(kfold.split(X)):
+        # Create model
+        model = Sequential()
+
+        model.add(Dense(10, activation="relu", input_dim=8))
+        model.add(Dense(1, activation="linear", input_dim=10))
+
+        # Compile model
+        def rmse(y_true, y_pred):
+            return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+        keras.optimizers.SGD(lr=0.08, momentum=0.2, decay=0.0, nesterov=False)
+        model.compile(loss='mean_squared_error', optimizer='sgd', metrics=[rmse])
+
+        # Fit model
+        model.fit(X[train], Y[train], epochs=500, batch_size=500, verbose=0)
+
+        # Evaluate model
+        scores = model.evaluate(X[test], Y[test], verbose=0)
+        rmseList.append(scores[0])
+        print("Fold :", i, " RMSE:", scores[0])
+
+        # print("RMSE: ", np.mean(rmseList))
     
 main()
