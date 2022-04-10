70fb5a65ef907ac06b23f17e35ffef516c83dfd7
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-10 19:26:54 +0300

    Updated report, main, phges

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 # from sklearn.preprocessing import MinMaxScaler, StandardScaler
 # from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import KFold
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Sequential
-
-import pandas as pd
+from sklearn.model_selection import KFold
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import backend as K
 
 def read_file(filename):
     arr = []
@@ -23,7 +24,7 @@ def read_file(filename):
 
 def read_data(fn1 , fn2, docs=None):
 
-    labels = read_file('Data/labels.txt')
+    # labels = read_file('Data/labels.txt')
     
     try:
         with open(fn1) as fd, open(fn2) as fl:
@@ -41,20 +42,16 @@ def read_data(fn1 , fn2, docs=None):
                 # get each line from the files
                 data_line = fd.readline().strip('\n')
                 label_line = fl.readline().strip('\n') 
-
+                
                 # check if EOF
                 if len(data_line) == 0:
                     return X, y
 
+                # labels
+                y.append(list(label_line.split(' ')))
+
                 # convert to list and throw 2 first elem
                 temp = data_line.split(' ')
-
-                # convert to list and search for '1s' indexes
-                ones = label_line.split(' ')
-                indices = [i for i in range(len(ones)) if ones[i] == '1']
-               
-                # get labels for every document
-                y.append([labels[index] for index in indices])
 
                 # get every word to create sentece
                 # The total of sentences create a document
@@ -75,16 +72,25 @@ def read_data(fn1 , fn2, docs=None):
         raise "Count not read file"
 
 
+def get_model(n_inputs, n_outputs, loss_f):
+	model = Sequential()
+	model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
+	model.add(Dense(n_outputs, activation='sigmoid'))
+	model.compile(loss=loss_f, optimizer='sgd')
+	return model
+
 def main():
    
     X, y = read_data('Data/test-data.dat', 'Data/test-label.dat', 10)
+  
     voc = [str(i) for i in range(8520)]
+
     # create vectorizer and pass existed corpus
     vectorizer = CountVectorizer(vocabulary=voc)
-    # nparray
-    X_bow = vectorizer.transform(X)
-    df_bow = pd.DataFrame(X_bow.toarray(), columns=voc)
-    df_bow['y'] = y
+
+    # tranform X, y to numpy arrays
+    X_bow = vectorizer.transform(X).toarray()
+    y = np.asarray(y, dtype=int)
     
     ################ NORMALIZATION ########################################
     
@@ -95,28 +101,25 @@ def main():
 
     # standardizer = StandardScaler()
     #print(standardizer.fit_transform(df_bow))
-    
 
     #################################### CROSS VALIDATION ################################
     # X_train, X_test, y_train, y_test = train_test_split(df_bow.iloc[:, :-1], df_bow['y'], test_size=0.4, random_state=0)
     # Split the data to training and testing data 5-Fold
 
+
+    # Split the data to training and testing data 5-Fold
     kfold = KFold(n_splits=5, shuffle=True)
     rmseList = []
-    rrseList = []
+    n_inputs = X_bow.shape
+    n_outputs = y.shape
 
-    for i, (train, test) in enumerate(kfold.split(X)):
-        # Create model
+    for i, (train, test) in enumerate(kfold.split(X_bow)):
         model = Sequential()
+        model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
+        model.add(Dense(n_outputs, activation='sigmoid'))
+        model.compile(loss=loss_f, optimizer='sgd')
 
-        model.add(Dense(10, activation="relu", input_dim=8))
-        model.add(Dense(1, activation="linear", input_dim=10))
-
-        # Compile model
-        def rmse(y_true, y_pred):
-            return K.sqrt(K.mean(K.square(y_pred - y_true)))
-
-        keras.optimizers.SGD(lr=0.08, momentum=0.2, decay=0.0, nesterov=False)
+        keras.optimizers.SGD(lr=1e-3, momentum=0.0, decay=0.0, nesterov=False)
         model.compile(loss='mean_squared_error', optimizer='sgd', metrics=[rmse])
 
         # Fit model
@@ -127,6 +130,6 @@ def main():
         rmseList.append(scores[0])
         print("Fold :", i, " RMSE:", scores[0])
 
-        # print("RMSE: ", np.mean(rmseList))
+    print("RMSE: ", np.mean(rmseList))
     
 main()
