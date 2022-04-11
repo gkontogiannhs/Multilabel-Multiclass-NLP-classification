a18f364a61c1d9400a360ae7a5972fd1136569f8
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-11 22:18:36 +0300

    Updated Report, main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import re
 from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 # from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import numpy as np
 import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras import backend as K
+import matplotlib.pyplot  as plt
+from sklearn.metrics import accuracy_score
+from numpy import mean
+from numpy import std
+
 
 def read_file(filename):
     arr = []
@@ -48,7 +50,7 @@ def read_data(fn1 , fn2, docs=None):
                     return X, y
 
                 # labels
-                y.append(list(label_line.split(' ')))
+                y.append(label_line.split(' '))
 
                 # convert to list and throw 2 first elem
                 temp = data_line.split(' ')
@@ -64,72 +66,57 @@ def read_data(fn1 , fn2, docs=None):
 
                 words.clear()
 
-                cnt += 1
-                if cnt >= docs:
-                    return X, y
+                # cnt += 1
+                # if cnt >= docs:
+                #    return X, y
                     
     except FileNotFoundError:
         raise "Count not read file"
 
 
-def get_model(n_inputs, n_outputs, loss_f):
-	model = Sequential()
-	model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
-	model.add(Dense(n_outputs, activation='sigmoid'))
-	model.compile(loss=loss_f, optimizer='sgd')
-	return model
-
 def main():
    
-    X, y = read_data('Data/test-data.dat', 'Data/test-label.dat', 10)
-  
+    X, y = read_data('Data/train-data.dat', 'Data/train-label.dat', 5000)
+
     voc = [str(i) for i in range(8520)]
 
     # create vectorizer and pass existed corpus
     vectorizer = CountVectorizer(vocabulary=voc)
 
     # tranform X, y to numpy arrays
-    X_bow = vectorizer.transform(X).toarray()
+    X = vectorizer.transform(X).toarray()
     y = np.asarray(y, dtype=int)
-    
+
     ################ NORMALIZATION ########################################
     
-    # normalizer = MinMaxScaler()
-    # print(normalizer.fit_transform(df_bow))
+    # X = MinMaxScaler().fit_transform(X)
 
     ################ STANDARDIZATION ########################################
+    # X = StandardScaler().fit_transform(X)
 
-    # standardizer = StandardScaler()
-    #print(standardizer.fit_transform(df_bow))
-
-    #################################### CROSS VALIDATION ################################
-    # X_train, X_test, y_train, y_test = train_test_split(df_bow.iloc[:, :-1], df_bow['y'], test_size=0.4, random_state=0)
-    # Split the data to training and testing data 5-Fold
-
+     # neural network's dims
+    input_dims, out_dims = X.shape[1], y.shape[1]
+    hidden_dims = 20
+    results = []
 
     # Split the data to training and testing data 5-Fold
-    kfold = KFold(n_splits=5, shuffle=True)
-    rmseList = []
-    n_inputs = X_bow.shape
-    n_outputs = y.shape
+    kfold = KFold(n_splits=2, shuffle=True)
+    for i, (train, test) in enumerate(kfold.split(X)):
+        
+        # create model
+        model = keras.Sequential([
+            keras.layers.Dense(hidden_dims, input_shape=(input_dims,), activation='relu'),
+            keras.layers.Dense(out_dims, activation='sigmoid')
+        ])
 
-    for i, (train, test) in enumerate(kfold.split(X_bow)):
-        model = Sequential()
-        model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
-        model.add(Dense(n_outputs, activation='sigmoid'))
-        model.compile(loss=loss_f, optimizer='sgd')
-
-        keras.optimizers.SGD(lr=1e-3, momentum=0.0, decay=0.0, nesterov=False)
-        model.compile(loss='mean_squared_error', optimizer='sgd', metrics=[rmse])
+        keras.optimizers.SGD(learning_rate=0.001)
+        model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['mse', 'accuracy'])
 
         # Fit model
-        model.fit(X[train], Y[train], epochs=500, batch_size=500, verbose=0)
+        model.fit(X[train], y[train], epochs=30, batch_size=10)
 
-        # Evaluate model
-        scores = model.evaluate(X[test], Y[test], verbose=0)
-        rmseList.append(scores[0])
-        print("Fold :", i, " RMSE:", scores[0])
+        #evalute model
+        print(i, model.evaluate(X[test], y[test]))
 
-    print("RMSE: ", np.mean(rmseList))
     
 main()
