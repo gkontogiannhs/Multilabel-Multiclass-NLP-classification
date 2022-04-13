92d9492c7b1200f61f692eddd92e1b7eed56ee7a
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-04-13 22:13:40 +0300

    Updated both
    Found the stupid error which caused the poor NN performance. keras.metrics.BinaryAccuracy(threshold=0.5)

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import numpy as np
-import keras
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Embedding
+from tensorflow import keras
 import matplotlib.pyplot  as plt
+from sklearn.metrics import accuracy_score
 
 # plot loss during training
-def plot(h):
-    plt.title('Loss / Mean Squared Error')
-    plt.plot(h.history['loss'], label='train')
-    plt.plot(h.history['val_loss'], label='test')
-    plt.legend()
+def plot(h, label):
+    plt.plot(h.history['binary_accuracy'])
+    plt.plot(h.history['val_binary_accuracy'])
+    plt.title(label + ' - Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
     plt.show()
+
+    # summarize history for loss
+    plt.plot(h.history['loss'])
+    plt.plot(h.history['val_loss'])
+    plt.title(label + ' - Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def myLoss(y_true, y_pred):
+    error = 0
+    for p, q in zip(y_true, y_pred):
+        if np.logical_or(p, q):
+            error += np.divide(np.logical_and(p, q), np.logical_or(p, q))
+    return error/len(y_true)
 
 def read_file(filename):
     arr = []
@@ -95,11 +112,19 @@ def read_data(fn1 , fn2, docs=None):
             plt.legend()
             plt.show()
 
+# get the model
+def get_model(n_inputs, n_outputs, n_hidden, loss_f):
+    model = keras.models.Sequential()
+    model.add(keras.Input(shape=(n_inputs,)))
+    model.add(keras.layers.Dense(n_hidden, activation='relu'))
+    model.add(keras.layers.Dense(n_outputs, activation='sigmoid'))
+    opt = keras.optimizers.SGD(learning_rate=1e-3)
+    model.compile(optimizer=opt, loss=loss_f, metrics=[keras.metrics.BinaryAccuracy(threshold=0.5)])
+    return model
 
 def main():
-    
     # load data
-    X, y = read_data('Data/train-data.dat', 'Data/train-label.dat', 8000)
+    X, y = read_data('Data/train-data.dat', 'Data/train-label.dat', 8250)
 
     # create corpus
     voc = [str(i) for i in range(8520)]
@@ -107,41 +132,35 @@ def main():
     # tranform X, y to numpy arrays
     X = CountVectorizer(vocabulary=voc).transform(X).toarray()
     y = np.asarray(y, dtype=int)
+
     
+    ################ CENTERING  ##########################################
+
     ################ NORMALIZATION ########################################
-    # X = MinMaxScaler().fit_transform(X)
+    X = MinMaxScaler().fit_transform(X)
 
     ################ STANDARDIZATION ########################################
     # X = StandardScaler().fit_transform(X)
-   
-    # model's dims
-    input_dims, out_dims = X.shape[1], y.shape[1]
-    hidden_dims = 8540
-    results = []
 
+    input("Tsekares ta labels twn graphs?")
+    input("tsekares to loss?")
+
+    history = []
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     # Split the data to training and testing data 5-Fold
     kfold = KFold(n_splits=5, shuffle=True)
-    for i, (train, test) in enumerate(kfold.split(X)):
-       
+    for i, (train, test) in enumerate(kfold.split(X_train)):
         # create model
-        model = Sequential([
-            Dense(hidden_dims, input_dim=input_dims, activation='relu'),
-            Dense(out_dims, activation='sigmoid')
-        ])
-
-        loss_fn = keras.losses.CategoricalCrossentropy()
-        opt = keras.optimizers.SGD(learning_rate=1e-3)
-        model.compile(loss=loss_fn, optimizer=opt, metrics=['accuracy'])
-
+        model = get_model(X.shape[1], y.shape[1], 4270, 'binary_crossentropy')
+   
         # Fit model
-        h = model.fit(X[train], y[train], epochs=50, batch_size=64, verbose=1)
+        h = model.fit(X_train[train], y_train[train], validation_data=(X_train[test], y_train[test]), epochs=50, batch_size=128, verbose=1)
+        history.append(h.history)
 
-        # evaluate the model
-        test_mse = model.evaluate(X[test], y[test], verbose=0)
-        print('Test: %.3f' % test_mse[0])
+        # evaluate model
+        model.evaluate(X_train[test], y_train[test])
 
-        results.append(test_mse[0])
+        plot(h, 'CE')
 
-        print("Fold :", i, " RMSE:", test_mse[0])
-    
+
 main()
